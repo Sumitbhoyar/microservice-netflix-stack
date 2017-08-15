d6b1be5d3542a0b8e530bdef8e368e33e08c39d2
Sumit Bhoyar <sumitbhoyar@gmail.com>
2017-08-15 22:18:29 +0530

    Enable docker

--- turbine/src/main/java/com/example/demo/TurbineServer.java
+++ turbine/src/main/java/com/example/demo/TurbineServer.java
@@ -3,13 +3,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
-@EnableHystrixDashboard
-public class DashboardServer {
+@EnableTurbine
+public class TurbineServer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DashboardServer.class, args);
+		SpringApplication.run(TurbineServer.class, args);
 	}
 }
--- turbine/src/test/java/com/example/demo/DashboardServerTests.java
+++ turbine/src/test/java/com/example/demo/DashboardServerTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ServiceApplicationTests {
+public class DashboardServerTests {
 
 	@Test
 	public void contextLoads() {
